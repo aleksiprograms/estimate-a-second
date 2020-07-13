b17b41066c553f33c0ebf663bb0577e0a5d33172
Aleksi Tolvanen <aleksiprograms@gmail.com>
2020-07-13 20:26:43 +0300

    Display alert if user wait too long

--- scripts.js
+++ scripts.js
@@ -1,18 +1,38 @@
 let timeStarted = false;
 let startTime = 0;
+let instruction = document.getElementById("instruction");
 let buttonStartAndStop = document.getElementById("buttonStartAndStop");
 let listResults = document.getElementById("listResults");
+let timeOut;
 
 const onButtonClick = (event) => {
     if (!timeStarted) {
-        startTime = Date.now();
-        buttonStartAndStop.innerHTML = "STOP";
+        start();
     } else {
-        buttonStartAndStop.innerHTML = "START";
+        stop(true);
+    }
+}
+
+const start = () => {
+    startTime = Date.now();
+    timeStarted = true;
+    buttonStartAndStop.innerHTML = "STOP";
+    instruction.innerHTML = "";
+    timeOut = setTimeout(() => {
+        instruction.innerHTML = "WAITED TOO LONG";
+        stop(false);
+    }, 10000);
+}
+
+const stop = (addToList) => {
+    if (addToList) {
         let result = ((Date.now() - startTime) / 1000).toFixed(3) + " s";
+        instruction.innerHTML = ""
         let li = document.createElement("li");
         li.appendChild(document.createTextNode(result));
         listResults.insertBefore(li, listResults.childNodes[0]);
     }
-    timeStarted = !timeStarted;
+    timeStarted = false;
+    buttonStartAndStop.innerHTML = "START";
+    clearTimeout(timeOut);
 }
