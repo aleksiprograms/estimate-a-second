bb08c9752996f93ab4673f73de7666d028bf07ec
Aleksi Tolvanen <aleksiprograms@gmail.com>
2020-07-13 17:23:15 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+let timeStarted = false;
+let startTime = 0;
+let buttonStartAndStop = document.getElementById("buttonStartAndStop");
+let result = document.getElementById("result");
+
+const onButtonClick = (event) => {
+    if (!timeStarted) {
+        startTime = Date.now();
+        buttonStartAndStop.innerHTML = "STOP";
+    } else {
+        buttonStartAndStop.innerHTML = "START";
+        result.innerHTML = Date.now() - startTime;
+    }
+    timeStarted = !timeStarted;
+}
